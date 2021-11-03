d76e7daded848aa1a9ffb9cd9b752c9fff191dcd
cerjakt <cerjak@dunwoody.edu>
2021-11-02 19:12:21 -0500

    updated my py files with functions

--- cylinder.py
+++ cylinder.py
@@ -1,15 +1,33 @@
-###################
-#
-# Name: Jake Cermak
-#
-# Program Name: Finding Volume and Surface Area of a Cylinder
-#
-# Program Description: A simple program that asks for the radius
-# and height to find the surface area, volume, lateral surface area,
-# and top or bottom surface area of a cylinder.
-#
-###################
+######################################################################
+#                                                                    #
+# Name: Jake Cermak                                                  #
+#                                                                    #
+# Program Name: Finding Volume and Surface Area of a Cylinder        #
+#                                                                    #
+# Program Description: A simple program that asks for the radius     #
+# and height to find the surface area, volume, lateral surface area, #
+# and top or bottom surface area of a cylinder.                      #
+#                                                                    #
+######################################################################
+
 import math
+
+def surfArea(radius, height):
+    surfArea = (2 * (math.pi) * (radius) * (height)) + (2 * (math.pi) * (radius)**2)
+    return surfArea
+
+def volume(radius, height):
+    volume = math.pi * radius * radius * height
+    return volume
+
+def latSurfArea(radius, height):
+    latSurfArea = 2 * (math.pi) * (radius) * (height)
+    return latSurfArea
+
+def topBotArea(radius, height):
+    topBotArea = (math.pi) * (radius)**2
+    return topBotArea
+
 def prompt():
     print("------------------------------------------------------------")
     print("PYTHON PROGRAM TO FIND VOLUME AND SURFACE AREA OF A CYLINDER")
@@ -17,18 +35,11 @@ def prompt():
     radius = eval(input("Please Enter the Radius: "))
     height = eval(input("Please Enter the Height: "))
     print()
-    surfarea = (2 * (math.pi) * (radius) * (height)) + (2 * (math.pi) * (radius)**2)
-    latsurfarea = 2 * (math.pi) * (radius) * (height)
-    topbotarea = (math.pi) * (radius)**2
-    print("The Surface Area of a Cylinder =",round(surfarea,2))
-    print("The Volume of a Cylinder = ", volume(radius, height))
-    print("Lateral Surface Area of a Cylinder =",round(latsurfarea,2))
-    print("Top or Bottom Surface Area of a Cylinder =",round(topbotarea,2))
+    print("The Surface Area of a Cylinder =", round(surfArea(radius, height),2 ))
+    print("The Volume of a Cylinder =", round(volume(radius, height), 2))
+    print("Lateral Surface Area of a Cylinder =", round(latSurfArea(radius, height),2 ))
+    print("Top or Bottom Surface Area of a Cylinder =", round(topBotArea(radius, height), 2))
     print("------------------------------------------------------------")
-
-def volume(radius, height):
-    volume = math.pi * radius * radius * height
-    return volume
 
 if __name__ == '__main__':
     prompt()
